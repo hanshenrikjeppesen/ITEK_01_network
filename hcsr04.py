dda33d6cb4f8feaebb3b3e969aa529d531382730
hanshenrikjeppesen <info@frist.dk>
2017-10-09 13:39:25 +0200

    new test file

--- hcsr04.py
+++ hcsr04.py
@@ -4,21 +4,24 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(16, GPIO.IN)
+TRIG = 23
+ECHO = 24
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
 
 def reading():
     pingtime = 0
     echotime = 0
-    GPIO.output(15,GPIO.LOW)
-    GPIO.output(15,GPIO.HIGH)
+    GPIO.output(TRIG,GPIO.LOW)
+    GPIO.output(TRIG,GPIO.HIGH)
     pingtime = time.time()
     time.sleep(.00001)
-    GPIO.output(15,GPIO.LOW)
-    while GPIO.input(16) == 0:
+    GPIO.output(TRIG,GPIO.LOW)
+    while GPIO.input(ECHO) == 0:
         pingtime = time.time()
-    while GPIO.input(16) == 1:
+    while GPIO.input(ECHO) == 1:
         echotime = time.time()
     if (echotime is not None) and (pingtime is not None):
         elapsedtime = echotime - pingtime
