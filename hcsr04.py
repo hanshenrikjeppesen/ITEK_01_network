6920e5a0d73686579bd2efc702a9f4378f422fc7
hanshenrikjeppesen <info@frist.dk>
2017-10-09 13:50:19 +0200

    new test file

--- hcsr04.py
+++ hcsr04.py
@@ -10,7 +10,33 @@ ECHO = 24
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
+print("distance measurement in progress")
 
+GPIO.output(TRIG, False)
+time.sleep(2)
+GPIO.output(TRIG, True)
+
+time.sleep(0.00001)
+
+GPIO.output(TRIG, False)
+
+while GPIO.input(ECHO) == 0:
+    pulse_start = time.time()
+
+while GPIO.input(ECHO) == 1:
+    pulse_end = time.time()
+
+pulse_duration = pulse_end - pulse_start
+
+distance = pulse_duration * 17150
+
+distance = round(distance, 2)
+
+print(distance)
+
+
+
+'''
 def reading():
     pingtime = 0
     echotime = 0
@@ -36,4 +62,5 @@ while True:
     print(distMeas)
     print("")
     time.sleep(0.25)
+'''
 
