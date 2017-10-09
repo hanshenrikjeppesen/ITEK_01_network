b98a15fc1b0e0e0e0c3e32b06609a4863d910b17
hanshenrikjeppesen <info@frist.dk>
2017-10-09 13:24:10 +0200

    new test file

--- hcsr04.py
+++ hcsr04.py
@@ -8,31 +8,28 @@ GPIO.setup(15, GPIO.OUT)
 GPIO.setup(16, GPIO.IN)
 
 
-def reading(sensor):
+def reading():
     pingtime = 0
     echotime = 0
-    if sensor == 0:
-        GPIO.output(15,GPIO.LOW)
-        GPIO.output(15,GPIO.HIGH)
+    GPIO.output(15,GPIO.LOW)
+    GPIO.output(15,GPIO.HIGH)
+    pingtime = time.time()
+    time.sleep(0.00001)
+    GPIO.output(15,GPIO.LOW)
+    while GPIO.input(16) == 0:
         pingtime = time.time()
-        time.sleep(0.00001)
-        GPIO.output(15,GPIO.LOW)
-        while GPIO.input(16) == 0:
-            pingtime = time.time()
-        while GPIO.input(16) == 1:
-            echotime = time.time()
-        if (echotime is not None) and (pingtime is not None):
-            elapsedtime = echotime - pingtime
-            distance = elapsedtime * 17000
-        else:
-            distance = 0
-        #print(pingtime)
-        #print(echotime)
-        return distance
+    while GPIO.input(16) == 1:
+        echotime = time.time()
+    if (echotime is not None) and (pingtime is not None):
+        elapsedtime = echotime - pingtime
+        distance = elapsedtime * 17000
+    else:
+        distance = 0
+    return distance
 
 
 while True:
-    distMeas = reading(0)
+    distMeas = reading()
     print(distMeas)
     print("")
     time.sleep(1)
