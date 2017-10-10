f8b2e9754629a1246666bfe93352b66c7e90d0e6
hanshenrikjeppesen <info@frist.dk>
2017-10-10 10:31:00 +0200

    new test file

--- hcsr04.py
+++ hcsr04.py
@@ -38,31 +38,3 @@ while True:
     distMeas()
     time.sleep(0.25)
 
-'''
-def reading():
-    pingtime = 0
-    echotime = 0
-    GPIO.output(TRIG,GPIO.LOW)
-    GPIO.output(TRIG,GPIO.HIGH)
-    pingtime = time.time()
-    time.sleep(.00001)
-    GPIO.output(TRIG,GPIO.LOW)
-    while GPIO.input(ECHO) == 0:
-        pingtime = time.time()
-    while GPIO.input(ECHO) == 1:
-        echotime = time.time()
-    if (echotime is not None) and (pingtime is not None):
-        elapsedtime = echotime - pingtime
-        distance = elapsedtime * 17000
-    else:
-        distance = 0
-    return distance
-
-
-while True:
-    distMeas = reading()
-    print(distMeas)
-    print("")
-    time.sleep(0.25)
-'''
-
